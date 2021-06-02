fd53c1c428c5e35b2d25f0321e89f8eba9fe87ad
ched88xtc <ched88official@gmail.com>
2021-06-02 14:55:21 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-document.querySelector('.header__burgermenu')
-  .addEventListener('click', function() {
-    document.querySelector('.burgermenu-icon').classList.toggle('active');
-    document.querySelector('.header__nav').classList.toggle('animate');
-    document.querySelector('.header__top').classList.toggle('top-active');
-  });
